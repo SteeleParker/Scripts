2caf57511281c831f9b3ec605e6b0a9a68d59067
SteeleParker <steele@sparkbusinesstechnology.com.au>
2020-03-30 23:20:46 +1000

    Create installCertbot.sh

---
+++ installCertbot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository -y ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install certbot -y
+sudo apt-get install python-certbot-apache -y
